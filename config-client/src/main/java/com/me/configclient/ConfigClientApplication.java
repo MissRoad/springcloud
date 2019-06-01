bed87c1c7527a96e36eb0abeead85196a3bd68ae
[lunars] <[1310811431@qq.com]>
2019-06-01 15:08:54 +0800

    8

--- config-client/src/main/java/com/me/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/me/configclient/ConfigClientApplication.java
@@ -3,6 +3,11 @@ package com.me.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -24,4 +32,15 @@ public class ConfigClientApplication {
     public String hi(){
         return foo;
     }
+
+    /**
+     * 在springboot的启动类中设置，一个配置文件中找不到继续找。
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }
+
 }
--- config-server/src/main/java/com/me/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/me/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.me.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
