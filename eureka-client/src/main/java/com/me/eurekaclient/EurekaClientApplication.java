ed0d45f1b92eab9874b04ff5bd4ad16ea21da38e
1310811431@qq.com <fzq1995->
2019-06-03 23:26:34 +0800

    添加   HystrixDashboard

--- eureka-client/src/main/java/com/me/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/me/eurekaclient/EurekaClientApplication.java
@@ -1,18 +1,33 @@
 package com.me.eurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 在程序的入口加上@EnableHystrix注解开启断路器，
+ * 这个是必须的，并且需要在程序中声明断路点HystrixCommand；
+ * 加上@EnableHystrixDashboard注解，开启HystrixDashboard
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
@@ -23,8 +38,25 @@ public class EurekaClientApplication {
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "spider") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+
+    /**
+     * 需要在服务实例（被监控服务）主类添加代码：
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> hystrixMetricsStreamServlet() {
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registration = new ServletRegistrationBean<>(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
     }
 
 }
