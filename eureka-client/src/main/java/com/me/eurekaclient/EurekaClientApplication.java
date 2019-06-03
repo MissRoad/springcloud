d3669dc00748aaafcedd8a294faa233326e4336d
1310811431@qq.com <fzq1995->
2019-06-03 22:51:45 +0800

    链路跟踪添加

--- eureka-client/src/main/java/com/me/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/me/eurekaclient/EurekaClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrixDashboard
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
